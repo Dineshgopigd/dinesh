74c1740887891f0e64bb4642f943e2b46495eb35
Dineshgopigd <94058518+Dineshgopigd@users.noreply.github.com>
2021-11-10 20:07:50 +0530

    Add files via upload
    ok

---
+++ Main.java
@@ -0,0 +1,55 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+package
+com.workattech.splitwise.models;
+
+public class User{
+  private String id;
+  private String name;
+  private String email;
+  private String phone;
+  
+  public User(string id, string name, string email, string phone){
+      this.id=id;
+      this.name=name;
+      this.email=email;
+      this.phone=phone;
+  }
+  
+  public string getld(){
+      return id;
+  }
+  
+  public void setld(string id){
+      this.id=id;
+  }
+  
+  public string getName(){
+      return name;
+  }
+  
+  public void setName(string name){
+      this.name=name;
+  }
+  
+  public string getEmail(){
+      return email;
+  }
+  
+  public void setEmail(string email){
+     thisemail=email; 
+  }
+  
+  public string getphone(){
+      return phone;
+  }
+  
+  public void setphone(string phone){
+      this.phone=phone;
+  }
+}
